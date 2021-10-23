b5f3628f0b0ebfef4d9d186a7e74a72ea5eec156
Maximilian118 <maxcrosby118@gmail.com>
2021-10-22 21:06:13 -0500

    TrackCard always with track.name

--- src/components/Cards/TrackCard/TrackCard.js
+++ src/components/Cards/TrackCard/TrackCard.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { nextTrack } from '../../../shared/utility'
 import StatsCard from '../StatsCard'
 import LineGraph from '../../Graphs/LineGraph'
 import MapBoxStatic from '../../Utility/MapBoxStatic'
 
 const TrackCard = ({ calendar }) => {
-  const [ imgErr, setImgErr ] = useState(!navigator.onLine)
   const track = nextTrack(calendar)
   
   return (
     <div className="card-model">
       <div className="top">
-        <h5>{`${track.isToday ? "Current" : "Next"} Track ${imgErr ? `- ${track.name}` : ``}`}</h5>
+        <h5>{`${track.isToday ? "Current" : "Next"} Track - ${track.name}`}</h5>
       </div>
-      {!imgErr && <img alt="Track Logo" src={track.logo} onError={e => setImgErr(true)}/>}
+      {navigator.onLine && <img alt="Track Logo" src={track.logo}/>}
       <StatsCard statsArr={track.trackStatsArr}/>
       {track.geojson.stats.elevation.elevArr.length > 0 &&  <LineGraph track={track} height={120}/>}
       {track.geojson && navigator.onLine && <MapBoxStatic geojson={track.geojson.geojson} width={240} height={200} highRes BRBot/>}
