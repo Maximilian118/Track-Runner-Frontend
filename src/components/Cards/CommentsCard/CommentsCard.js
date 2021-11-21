0e6319d0dda2a682dc6301a790efac670391f2f2
Maximilian118 <maxcrosby118@gmail.com>
2021-11-21 16:07:37 +0300

    CommentsCard with Input

--- src/components/Cards/CommentsCard/CommentsCard.js
+++ src/components/Cards/CommentsCard/CommentsCard.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import './_CommentsCard.scss'
+import { TextField } from '@mui/material'
 
 const CommentsCard = () => (
   <div className="comments-card">
-
+    <TextField
+      size="small"
+      variant="outlined"
+      label="Write a comment..."
+      name="comment"
+      className="mui-text-field-comment"
+      onClick={e => e.stopPropagation()}
+    />
   </div>
 )
 
