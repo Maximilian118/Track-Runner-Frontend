9b1904f6e365d2a6c601838367e5416c563af88d
Maximilian118 <maxcrosby118@gmail.com>
2021-12-05 19:19:37 +0300

    SendBtn

--- src/components/Cards/CommentsCard/CommentsCard.js
+++ src/components/Cards/CommentsCard/CommentsCard.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './_CommentsCard.scss'
-import { TextField } from '@mui/material'
+import { TextField, Button } from '@mui/material'
 
-const CommentsCard = () => (
-  <div className="comments-card">
-    <TextField
-      size="small"
-      variant="outlined"
-      label="Write a comment..."
-      name="comment"
-      className="mui-text-field-comment"
-      onClick={e => e.stopPropagation()}
-    />
-  </div>
-)
+const CommentsCard = () => {
+  const [ sendBtn, setSendBtn ] = useState(false)
+
+  const handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      sendBtn && setSendBtn(false)
+    } else {
+      !sendBtn && setSendBtn(true)
+    }
+  }
+
+  return (
+    <div className="comments-card">
+      <TextField
+        size="small"
+        variant="outlined"
+        label="Write a comment..."
+        name="comment"
+        className="mui-text-field-comment"
+        onClick={e => e.stopPropagation()}
+        InputProps={{
+          endAdornment: <Button
+            variant="text"
+            className="mui-input-btn"
+            style={{ opacity: sendBtn ? 1 : 0 }}
+            onClick={e => sendBtn && setSendBtn(false)}
+          >Send</Button>
+        }}
+        onKeyPress={e => handleKeyPress(e)}
+        onChange={e => e.target.value.trim() === "" && sendBtn && setSendBtn(false)}
+      />
+    </div>
+  )
+}
 
 export default CommentsCard
