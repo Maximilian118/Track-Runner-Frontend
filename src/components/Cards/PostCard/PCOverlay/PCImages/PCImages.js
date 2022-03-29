da819fab63255ffb9eb1ae0d1fb1c43f45fc926e
Maximilian118 <maxcrosby118@gmail.com>
2022-03-29 19:13:41 +0100

    No background: white on img-containers

--- src/components/Cards/PostCard/PCOverlay/PCImages/PCImages.js
+++ src/components/Cards/PostCard/PCOverlay/PCImages/PCImages.js
@@ -5,24 +5,29 @@ import { Gesture } from '@mui/icons-material'
 const PCImages = ({ imgs, setImg, setImgHeight }) => {
   const [ currentImg, setCurrentImg ] = useState(null)
 
-  const imgClickedHandler = (e, img) => {
+  const imgClickedHandler = (e, img, isGesture) => {
     e.stopPropagation()
-    setImg(currentImg === img ? null : <img alt="Post" style={{ width: 440 }} src={img} onLoad={(e) => setImgHeight(e.target.clientHeight - 2)}/>)
-    setCurrentImg(currentImg === img ? null : img)
-    currentImg === img && setImgHeight(180)
+    setImg(isGesture ? null : <img alt="Post" style={{ width: 440 }} src={img} onLoad={(e) => setImgHeight(e.target.clientHeight - 2)}/>)
+    setCurrentImg(isGesture ? null : img)
+    isGesture && setImgHeight(180)
   }
 
   return (
     <div className="post-card-imgs">
-      {imgs.map((img, i) => 
-        <div 
-          key={i} 
-          className="post-card-img-container"
-          onClick={e => imgClickedHandler(e, img)}
-        >
-          {currentImg === img ? <Gesture/> : <img alt="run" src={img}/>}
-        </div>
-      )}
+      {imgs.map((img, i) => {
+        const isGesture = currentImg === img
+
+        return (
+          <div 
+            key={i} 
+            className="post-card-img-container"
+            onClick={e => imgClickedHandler(e, img, isGesture)}
+            style={{ background: isGesture ? "white" : "none" }}
+          >
+            {isGesture ? <Gesture/> : <img alt="run" src={img}/>}
+          </div>
+        )
+      })}
     </div>
   )
 }
